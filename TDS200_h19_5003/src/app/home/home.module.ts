f765ef47f4b3f430a106d2773deff0614ff1a329
Markus Jahnsrud <markus@soriko.no>
2019-11-05 10:53:51 +0100

    Added Meeting Room component and Auth service

--- TDS200_h19_5003/src/app/home/home.module.ts
+++ TDS200_h19_5003/src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
+import {MeetingRoomComponent} from '../components/meeting-room/meeting-room.component';
 
 @NgModule({
   imports: [
@@ -18,6 +19,6 @@ import { HomePage } from './home.page';
       }
     ])
   ],
-  declarations: [HomePage]
+    declarations: [HomePage, MeetingRoomComponent]
 })
 export class HomePageModule {}
