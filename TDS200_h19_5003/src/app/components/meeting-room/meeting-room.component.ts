c71528b1e2f4424fa1db733b9f8e541758b900fd
Markus Jahnsrud <markus@soriko.no>
2019-11-08 18:36:49 +0100

    Configuration work

--- TDS200_h19_5003/src/app/components/meeting-room/meeting-room.component.ts
+++ TDS200_h19_5003/src/app/components/meeting-room/meeting-room.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-meeting-room',
@@ -7,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MeetingRoomComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {}
+
+  openMeetingRoom() {
+    this.router.navigate(['room']);
+  }
 
 }
