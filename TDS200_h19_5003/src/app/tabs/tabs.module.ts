08e90050d3c081a13f8acbce659e94d31d20692b
Markus Jahnsrud <markus@soriko.no>
2019-11-13 21:42:45 +0100

    Polish

--- TDS200_h19_5003/src/app/tabs/tabs.module.ts
+++ TDS200_h19_5003/src/app/tabs/tabs.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       { path: 'tab1', loadChildren: '../tab-discovery/tab-discovery.module#TabDiscoveryPageModule' },
-      { path: 'tab2', loadChildren: '../tab2/tab2.module#Tab2PageModule' },
+      { path: 'tab2', loadChildren: '../my-rooms/my-rooms.module#MyRoomsPageModule' },
       { path: 'tab3', loadChildren: '../tab3/tab3.module#Tab3PageModule' },
     ]
   },
